9cff825d85488f6204f3539ee7def534e5b3fff9
Petrica Nanca <petrica_nanca@yahoo.com>
2018-05-10 20:11:38 +0300

    Complete validation and the forms

--- js/app.js
+++ js/app.js
@@ -49,19 +49,29 @@ var resetData = function() {
     };
 }
 
-var firstForm     = document.getElementById('modal-form-section-one');
-var secondForm    = document.getElementById('modal-form-section-two');
-var showSubmitted = document.getElementById('show-submitted-data');
+var firstForm        = document.getElementById('modal-form-section-one');
+var secondForm       = document.getElementById('modal-form-section-two');
+var showSubmitted    = document.getElementById('show-submitted-data');
 
 /**
  * Fields
  */
-var titleField   = document.getElementById('title-anouncement');
-var brandField   = document.getElementById('mark-brand');
-var yearField    = document.getElementById('year');
-var mileageField = document.getElementById('mileage');
-var fuelField    = document.getElementById('fuel');
-var colorField   = document.getElementById('color');
+var titleField       = document.getElementById('title-anouncement');
+var brandField       = document.getElementById('mark-brand');
+var yearField        = document.getElementById('year');
+var mileageField     = document.getElementById('mileage');
+var colorField       = document.getElementById('color');
+var selectedColor    = document.getElementById('selected-color');
+var damageNo         = document.getElementById('damage-no');
+var damageYes        = document.getElementById('damage-yes');
+var damageDetailsBox = document.getElementById('damage-details-box');
+var damageDetails    = document.getElementById('damage-details');
+var priceField       = document.getElementById('price');
+var currencyField    = document.getElementById('currency');
+var descriptionField = document.getElementById('description');
+
+var errorMessages    = document.getElementById('errors');
+var errorMessages2   = document.getElementById('errors-two');
 
 // Populate year
 var yearNow = parseInt((new Date()).getFullYear());
@@ -93,47 +103,181 @@ var updateImage = function(autoItem) {
 
 // Update the color
 var updateColor = function(colorField) {
-    var selectedColor = document.getElementById('selected-color');
     selectedColor.style.backgroundColor = colorField.value;
-    console.log(colorField.value);
 };
 
 
 /**
  *  Validate first form
  */
+var validateFirstForm = function() {
+    errorMessages.innerHTML     = ''; // clear error firsts
+    if(titleField.value == '') {                                   // title validation
+        titleField.classList.add('form-error');
+        titleField.focus();
+        errorMessages.innerHTML += '<br /> Title cannot be empty.';
+        errorMessages.style.display = 'block';
+        return false;
+    } else {
+        titleField.classList.remove('form-error');
+    }
 
+    if(brandField.value == '-- Select a brand --') {              // brand validation
+        brandField.classList.add('form-error');
+        brandField.focus();
+        errorMessages.innerHTML += '<br /> You must select the brand of the car.';
+        errorMessages.style.display = 'block';
+        return false;
+    } else {
+        brandField.classList.remove('form-error');
+    }
+
+    var regex=/^[0-9]+$/;
+    var mileageVal = mileageField.value;
+    if(mileageField.value == '' || !mileageVal.match(regex)) {    // mileage validation
+        mileageField.classList.add('form-error');
+        mileageField.focus();
+        errorMessages.innerHTML += '<br /> Mileage must not be empty and must be a number.';
+        errorMessages.style.display = 'block';
+        return false;
+    } else {
+        mileageField.classList.remove('form-error');
+    }
+
+    if(colorField.value == '') {                                  // color validation
+        colorField.classList.add('form-error');
+        colorField.focus();
+        errorMessages.innerHTML += '<br /> You must set the color.';
+        errorMessages.style.display = 'block';
+        return false;
+    } else {
+        brandField.classList.remove('form-error');
+    }
+
+    errorMessages.innerHTML     = '';                             // clear errors
+    errorMessages.style.display = 'none';
+    return true;
+};
 
 /**
  * Validate second form
  */
+var validateSecondForm = function() {
+    errorMessages2.innerHTML     = '';
+    if (damageYes.checked && damageDetails.value == '') {
+        damageDetails.classList.add('form-error');
+        damageDetails.focus();
+        errorMessages2.innerHTML += '<br /> You must provide damage details.';
+        errorMessages2.style.display = 'block';
+        return false;
+    } else {
+        damageDetails.classList.remove('form-error');
+    }
+
+    var priceRegex = /^[1-9][0-9]{0,2}(?:\.?[0-9]{3}){0,3}(,[0-9]{2})?$/;
+    var priceVal = priceField.value;
+    if(priceField.value == '' || !priceVal.match(priceRegex)) {    // price validation
+        priceField.classList.add('form-error');
+        priceField.focus();
+        errorMessages2.innerHTML += '<br /> You must provide a correct price.';
+        errorMessages2.style.display = 'block';
+        return false;
+    } else {
+        priceField.classList.remove('form-error');
+    }
+
+    if(descriptionField.value == '') {                             // description validation
+        descriptionField.classList.add('form-error');
+        descriptionField.focus();
+        errorMessages2.innerHTML += '<br /> Description cannot be empty.';
+        errorMessages2.style.display = 'block';
+        return false;
+    } else {
+        descriptionField.classList.remove('form-error');
+    }
+
+    errorMessages2.innerHTML     = '';                             // clear errors
+    errorMessages2.style.display = 'none';
+    return true;
+}
 
 
-
-
-
+/**
+ *  Render second form
+ */
 var renderSecondForm = function() {
+    updateDamage();
     //validate first form
-    firstForm.classList.add('hidden');
-    secondForm.classList.remove('hidden');
+    if (validateFirstForm()) {
+        firstForm.classList.add('hidden');
+        secondForm.classList.remove('hidden');
+    }
 };
 
 
+/**
+ * Initial setup
+ */
 var initialSetup = function() {
     resetData();
-    var titleField = document.getElementById('modal-form-section-one');
+    titleField.value   = '';
+    brandField.value   = '-- Select a brand --';
+    yearField.value    = yearNow;
+    mileageField.value = '';
+    colorField.value   = '';
+    selectedColor.style.backgroundColor = 'white';
+
+    damageNo.checked       = true;
+    damageDetails.value    = '';
+    priceField.value       = '';
+    descriptionField.value = '';
+    firstForm.classList.remove('hidden');
+    secondForm.classList.add('hidden');
+    showSubmitted.classList.add('hidden');
 }
+
+/**
+ * updateDamage
+ */
+var updateDamage = function() {
+    damageDetailsBox.style.display = (damageYes.checked) ? 'block' : 'none';
+    if (damageYes.checked) damageDetails.focus();
+}
+
+
+/**
+ * Cancel
+ */
+var cancelSubmission = function() {
+    firstForm.classList.remove('hidden');
+    secondForm.classList.add('hidden');
+    showSubmitted.classList.add('hidden');
+};
 
 /**
  * Complete submission
  */
 var completeSubmission = function() {
    // validate second form
-   var collectedData = JSON.stringify(dataHolder);
-   showSubmitted.innerHTML = '<br /> Collected Data => <br />' + collectedData;
-   firstForm.classList.add('hidden');
-   secondForm.classList.add('hidden');
-   showSubmitted.classList.remove('hidden');
+   if (validateSecondForm()) {
+       dataHolder.title          = titleField.value;
+       dataHolder.brand          = brandField.value;
+       dataHolder.year           = yearField.value;
+       dataHolder.mileage        = mileageField.value;
+       dataHolder.fuel           = document.querySelector('input[name="fuel"]:checked').value;
+       dataHolder.color          = colorField.value;
+       dataHolder.damaged        = document.querySelector('input[name="damaged"]:checked').value
+       dataHolder.damage_details = damageDetails.value;
+       dataHolder.price          = priceField.value;
+       dataHolder.currency       = currencyField.value;
+       dataHolder.description    = descriptionField.value;
+
+       var collectedData = JSON.stringify(dataHolder);
+       showSubmitted.innerHTML = '<br /> Collected Data => <br /><br />' + collectedData;
+       firstForm.classList.add('hidden');
+       secondForm.classList.add('hidden');
+       showSubmitted.classList.remove('hidden');
+   }
 }
 
 
@@ -144,11 +288,13 @@ var completeSubmission = function() {
 var overlay = document.getElementById('overlay');
 
 var openModal = function () {
+    initialSetup();
 	overlay.classList.remove("is-hidden");
 }
 
 var closeModal = function () {
 	overlay.classList.add("is-hidden");
+	initialSetup();
 }
 
 
